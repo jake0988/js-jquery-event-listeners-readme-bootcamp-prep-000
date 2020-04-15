b02308c9b38b7576725a853910cb4db10651712e
Jake G <jake098877@gmail.com>
2020-04-15 16:52:03 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,39 +1,44 @@
 function getIt() {
-  alert("Hey!")
-}
 
-function frameIt() {
- $("img").addClass("tasty");
-  'img'.class ='tasty'
-}
-
-function pressIt() {
-    $('#typing').on('keydown', function(e)) {
-      console.log(e);
-  if $('G') {
-    alert('You pressed a G key')
-
-  }
-}
-
-
+  $('p').on('click', function(){
+      alert("Hey!")
+  })
 
 
 }
-function submitIt() {
-  alert("Your form is going to be submitted now.")
-  return
-}
+
+// /*function frameIt() {
+//  $("img").addClass("tasty");
+//   'img'.class ='tasty'
+// }
+//
+// function pressIt() {
+//     $('#typing').on('keydown', function(e)) {
+//       console.log(e);
+//   if $('G') {
+//     alert('You pressed a G key')
+//
+//   }
+// }
+//
+//
+//
+//
+// }
+// function submitIt() {
+//   alert("Your form is going to be submitted now.")
+//   return
+// }
 
 $(document).ready(function(){
-
-  $('p').on('click',getIt())
-
-  $('img').on('load', frameIt())
-
-  //$("input").on('keydown', pressIt())
-
-
-  $("form").on("submit", submitIt())
+  //
+  // $('p').on('click',getIt())
+  //
+  // $('img').on('load', frameIt())
+  //
+  // //$("input").on('keydown', pressIt())
+  //
+  //
+  // $("form").on("submit", submitIt())
 
 });
